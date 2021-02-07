1c6f874c0b2532086e78ff0ab1198ff1fc227065
sudmukhe <sudeshna1india@gmail.com>
2021-02-06 23:19:27 -0800

    Add files via upload

---
+++ src/main/java/com/example/OurNewApplication/OurNewApplication.java
@@ -0,0 +1,13 @@
+package com.example.OurNewApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurNewApplication.class, args);
+	}
+
+}
